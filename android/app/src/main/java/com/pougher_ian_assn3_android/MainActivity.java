74515dcf1cd5899fc3b8f88f8c48ab4814d57c2c
Ianpougher <ianpougher@gmail.com>
2018-01-27 20:20:09 -0700

    initial commit

---
+++ android/app/src/main/java/com/pougher_ian_assn3_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pougher_ian_assn3_android;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Pougher_Ian_Assn3_Android";
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { AppRegistry } from 'react-native';
+import App from './components/App.js';
+AppRegistry.registerComponent('Pougher_Ian_Assn3_Android', () => App );
